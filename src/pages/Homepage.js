7cc981ceebef9a92f73e2acac89ab74ce5407dc0
Onoja Victor <onoja.jsdev@gmail.com>
2023-02-12 15:55:45 +0100

    Add get all products input and output fileds

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,10 @@
 import Navigation from "../components/Navigation";
 import Sidebar from "../components/Sidebar";
 import "../styles/homepage.scss"
+import { BiCube } from "react-icons/bi"
 export default function Homepage() {
     return (
-        <> 
+        <>
             <div className="homepage-container">
                 <div className="sidebar">
                     <Sidebar />
@@ -11,7 +12,51 @@ export default function Homepage() {
                 <div className="content">
                     <Navigation />
                     <div className="body">
-                        other part
+                        <div className="products">
+                            <h2> <BiCube /> Products</h2>
+
+                            <div className="all">
+                                <h3>Get all products</h3>
+                                <div className="input-section">
+                                    <div className="input">
+
+                                        {`  fetch('https://fakestoreapi.com/products')
+
+`}
+                                        {`.then(res=>res.json()) `}
+                                        {`.then(json=>console.log(json))`}
+                                    </div>
+                                </div>
+<button className="hide-btn">
+    Hide output
+</button>
+                                <div className="output-section">
+                                    <div className="output">
+                                        {`     //Output
+                                    [
+                                    {
+                                        id:1,
+                                    title:'...',
+                                    price:'...',
+                                    category:'...',
+                                    description:'...',
+                                    image:'...'
+                },
+                                    /*...*/
+                                    {
+                                        id:30,
+                                    title:'...',
+                                    price:'...',
+                                    category:'...',
+                                    description:'...',
+                                    image:'...'
+                }
+                                    ]`}
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
